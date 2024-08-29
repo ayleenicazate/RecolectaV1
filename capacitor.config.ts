51f0f20724c3b3f3e89c84c7261f90e609b6cba6
Ayleen <ayl.guajardo@duocuc.cl>
2024-08-29 11:10:22 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'recolectav1',
+  webDir: 'www'
+};
+
+export default config;
